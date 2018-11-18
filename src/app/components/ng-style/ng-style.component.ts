bc87eb2be9320b5a83c7544eb63ecb06616a3bad
carlos sk <skyrko.carlos.sk@gmail.com>
2018-11-18 00:10:58 -0600

    ngStyle

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  templateUrl: './ng-style.component.html',
+  styleUrls: ['./ng-style.component.css']
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano = 10;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
